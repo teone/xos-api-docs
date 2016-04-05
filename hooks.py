77e68dd6c3d49c3f112b819f3efb424208d89b4e
Matteo Scandolo <teo@onlab.us>
2016-04-05 14:12:40 -0700

    vOLT and test

--- hooks.py
+++ hooks.py
@@ -8,6 +8,26 @@ restoreSubscriber = "ssh xos 'sudo docker exec frontend_xos_db_1 psql -U postgre
 
 @hooks.before_each
 def my_before_each_hook(transaction):
-    print('before each restore')
+    # print('before each restore', transaction['name'])
     commands.getstatusoutput(restoreSubscriber)
     sys.stdout.flush()
+
+
+@hooks.before("Truckroll > Truckroll Collection > Create a Truckroll")
+def skip_test1(transaction):
+    transaction['skip'] = True
+
+
+@hooks.before("Truckroll > Truckroll Detail > View a Truckroll Detail")
+def skip_test2(transaction):
+    transaction['skip'] = True
+
+
+@hooks.before("Truckroll > Truckroll Detail > Delete a Truckroll Detail")
+def skip_test3(transaction):
+    transaction['skip'] = True
+
+
+@hooks.before("vOLT > vOLT Collection > Create a vOLT")
+def skip_test4(transaction):
+    transaction['skip'] = True
